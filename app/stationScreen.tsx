a1cdb0bc3a0d26c7f7754055d1eed7b61b189d80
gojaime <gojaime@up.edu.ph>
2025-04-12 11:11:46 +0800

    Updated stations description

--- app/stationScreen.tsx
+++ app/stationScreen.tsx
@@ -14,7 +14,7 @@ export default function TabTwoScreen() {
   const {  } = useLocalSearchParams();
   const router = useRouter();
 
-  const { destinationIndex, setDestinationIndex, setDirection, setNextStation, nextStation, latitude,longitude,sbstations,nbstations,currentStation } = useGlobalState();
+  const { destinationIndex, setDestinationIndex, setDirection, setNextStation, nextStation, latitude,longitude,sbstations,nbstations,currentStation, stationsDesc } = useGlobalState();
 
   const newIndexParsed = Number(newIndex);
 
@@ -76,6 +76,9 @@ export default function TabTwoScreen() {
     console.log("nb");
   }
 
+  const station = stationsDesc.find(station => station.id === stations[newIndexParsed].id);
+
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#D0D0D0' }}
@@ -88,7 +91,7 @@ export default function TabTwoScreen() {
         />
       }>
       <View style={{backgroundColor: 'white'}}>
-        <ThemedText type="title" darkColor='dark'>{post}</ThemedText>
+        <ThemedText type="title" darkColor='dark'>{stations[newIndexParsed].name}</ThemedText>
         <View style={{flexDirection: 'row', justifyContent: 'flex-start', marginTop: 10}}>
           {<TouchableOpacity
             style={{
@@ -111,12 +114,28 @@ export default function TabTwoScreen() {
             </TouchableOpacity>}
           <TouchableOpacity style={{backgroundColor: '#fcd20f', borderRadius: 10, padding: 5, flex: 25, justifyContent: 'center', alignItems: 'center'}}><Entypo name="map" size={24} color="black" /></TouchableOpacity>
         </View>
-        <View style={{marginTop: 20}}>
-        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
-        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
-        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
-        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
-        </View>
+
+      {station?         <View style={{margin: 10}}>
+        <Text style={{ fontSize: 18, fontWeight: '600', marginTop: 10 }}>Description:</Text>
+      {station!.intro.map((line, index) => (
+        <Text key={index} style={{ marginLeft: 10, marginTop: 2 }}>{`\u2022 ${line}`}</Text>
+      ))}
+
+      <Text style={{ fontSize: 18, fontWeight: '600', marginTop: 10 }}>Dining Options Nearby:</Text>
+      {station!.food.map((item, index) => (
+        <Text key={index} style={{ marginLeft: 10, marginTop: 2 }}>{`\u2022 ${item}`}</Text>
+      ))}
+
+      <Text style={{ fontSize: 18, fontWeight: '600', marginTop: 10 }}>Places to Visit:</Text>
+      {station!.places.map((place, index) => (
+        <Text key={index} style={{ marginLeft: 10, marginTop: 2 }}>{`\u2022 ${place}`}</Text>
+      ))}
+
+      <Text style={{ fontSize: 18, fontWeight: '600', marginTop: 10 }}>Accommodation:</Text>
+      {station!.accommodation.map((acc, index) => (
+        <Text key={index} style={{ marginLeft: 10, marginTop: 2 }}>{`\u2022 ${acc}`}</Text>
+      ))}
+        </View> : <Text style={{margin: 10}}>No available description yet</Text>}
       </View>
     </ParallaxScrollView>
   );
